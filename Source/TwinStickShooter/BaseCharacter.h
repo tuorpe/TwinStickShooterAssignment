606e1a63a9fa83dcf14cc24440e985647c5a1fd6
Peter Tuoriniemi <fpetu005@edu.xamk.fi>
2024-12-03 17:24:13 +0200

    Initial commit

---
+++ Source/TwinStickShooter/BaseCharacter.h
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "BaseCharacter.generated.h"
+
+UCLASS(Blueprintable)
+class TWINSTICKSHOOTER_API ABaseCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Make a healthproperty
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Base Character")
+	float Health = 100;
+	// Make an isDead property
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Base Character")
+		bool isDead = false;
+
+	// Calculate death function (helper)
+	virtual void CalculateDead();
+
+	//Calculate health function
+	UFUNCTION(BlueprintCallable, Category = "Base Character")
+		virtual void CalculateHealth(float delta);
+
+	//Editor-centric code for changing properties
+	#if WITH_EDITOR
+		virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
+#endif
+public:
+	// Sets default values for this character's properties
+	ABaseCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
